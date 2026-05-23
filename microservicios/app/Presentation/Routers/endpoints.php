6265fc84094d5f092a5350645bb31f4b20114e3f
Nicolas <jnjimenez@uniboyaca.edu.co>
2026-05-22 21:49:39 -0500

    actualización endpoints

--- microservicios/app/Presentation/Routers/endpoints.php
+++ microservicios/app/Presentation/Routers/endpoints.php
@@ -1,101 +1,47 @@
 <?php
 
-use App\Controllers\RetrospectivosController;
-use App\Controllers\ItemsController;
-use App\Controllers\SeguimientoController;
+use App\Repositories\RetrospectivosRepository;
+use App\Repositories\ItemsRepository;
+use App\Repositories\SeguimientoRepository;
+use Slim\App;
+use Slim\Routing\RouteCollectorProxy;
 
-return function ($app) {
+return function (App $app) {
 
     // ─── RETROSPECTIVAS ───────────────────────────────────────────
-    $app->get('/retrospectivas', function ($request, $response) {
-        $controller = new RetrospectivosController();
-        $data = $controller->getRetrospectivas();
-        $response->getBody()->write(json_encode($data));
-        return $response->withHeader('Content-Type', 'application/json');
-    });
-
-    $app->get('/retrospectivas/{id}', function ($request, $response, $args) {
-        $controller = new RetrospectivosController();
-        $data = $controller->getRetrospectiva($args['id']);
-        $response->getBody()->write(json_encode($data));
-        return $response->withHeader('Content-Type', 'application/json');
-    });
-
-    $app->post('/retrospectivas', function ($request, $response) {
-        $controller = new RetrospectivosController();
-        $data = $request->getParsedBody();
-        $result = $controller->guardarRetrospectiva($data);
-        $response->getBody()->write(json_encode($result));
-        return $response->withHeader('Content-Type', 'application/json')->withStatus(201);
-    });
-
-    $app->put('/retrospectivas/{id}', function ($request, $response, $args) {
-        $controller = new RetrospectivosController();
-        $data = $request->getParsedBody();
-        $result = $controller->modificarRetrospectiva($args['id'], $data);
-        $response->getBody()->write(json_encode($result));
-        return $response->withHeader('Content-Type', 'application/json');
-    });
-
-    $app->delete('/retrospectivas/{id}', function ($request, $response, $args) {
-        $controller = new RetrospectivosController();
-        $result = $controller->borrarRetrospectiva($args['id']);
-        $response->getBody()->write(json_encode($result));
-        return $response->withHeader('Content-Type', 'application/json');
-    });
+    $app->get('/retrospectivas', [RetrospectivosRepository::class, 'list']);
+    $app->post('/retrospectiva', [RetrospectivosRepository::class, 'create']);
+    $app->get('/retrospectiva/{id}', [RetrospectivosRepository::class, 'detail']);
+    $app->put('/retrospectiva/{id}', [RetrospectivosRepository::class, 'update']);
+    $app->delete('/retrospectiva/{id}', [RetrospectivosRepository::class, 'delete']);
 
     // ─── ITEMS (logros, impedimentos, acciones) ───────────────────
-    $app->get('/retrospectivas/{id}/items', function ($request, $response, $args) {
-        $controller = new ItemsController();
-        $data = $controller->getItems($args['id']);
-        $response->getBody()->write(json_encode($data));
-        return $response->withHeader('Content-Type', 'application/json');
-    });
-
-    $app->post('/retrospectivas/{id}/items', function ($request, $response, $args) {
-        $controller = new ItemsController();
-        $data = $request->getParsedBody();
-        $result = $controller->guardarItem($args['id'], $data);
-        $response->getBody()->write(json_encode($result));
-        return $response->withHeader('Content-Type', 'application/json')->withStatus(201);
-    });
-
-    $app->put('/items/{id}', function ($request, $response, $args) {
-        $controller = new ItemsController();
-        $data = $request->getParsedBody();
-        $result = $controller->modificarItem($args['id'], $data);
-        $response->getBody()->write(json_encode($result));
-        return $response->withHeader('Content-Type', 'application/json');
-    });
-
-    $app->delete('/items/{id}', function ($request, $response, $args) {
-        $controller = new ItemsController();
-        $result = $controller->borrarItem($args['id']);
-        $response->getBody()->write(json_encode($result));
-        return $response->withHeader('Content-Type', 'application/json');
-    });
+    $app->get('/retrospectiva/{id}/items', [ItemsRepository::class, 'list']);
+    $app->post('/retrospectiva/{id}/items', [ItemsRepository::class, 'create']);
+    $app->put('/item/{id}', [ItemsRepository::class, 'update']);
+    $app->delete('/item/{id}', [ItemsRepository::class, 'delete']);
 
     // ─── SEGUIMIENTO DE ACCIONES ──────────────────────────────────
-    $app->get('/retrospectivas/{id}/acciones-anteriores', function ($request, $response, $args) {
-        $controller = new SeguimientoController();
-        $data = $controller->getAccionesAnteriores($args['id']);
-        $response->getBody()->write(json_encode($data));
-        return $response->withHeader('Content-Type', 'application/json');
-    });
+    $app->get('/retrospectiva/{id}/acciones-anteriores', [SeguimientoRepository::class, 'accionesAnteriores']);
+    $app->post('/seguimiento', [SeguimientoRepository::class, 'create']);
+    $app->put('/seguimiento/{id}', [SeguimientoRepository::class, 'update']);
 
-    $app->post('/seguimiento', function ($request, $response) {
-        $controller = new SeguimientoController();
-        $data = $request->getParsedBody();
-        $result = $controller->guardarSeguimiento($data);
-        $response->getBody()->write(json_encode($result));
-        return $response->withHeader('Content-Type', 'application/json')->withStatus(201);
-    });
+    // ─── AGRUPADO /api ────────────────────────────────────────────
+    $app->group('/api', function (RouteCollectorProxy $group) {
 
-    $app->put('/seguimiento/{id}', function ($request, $response, $args) {
-        $controller = new SeguimientoController();
-        $data = $request->getParsedBody();
-        $result = $controller->modificarSeguimiento($args['id'], $data);
-        $response->getBody()->write(json_encode($result));
-        return $response->withHeader('Content-Type', 'application/json');
+        $group->get('/retrospectivas', [RetrospectivosRepository::class, 'list']);
+        $group->post('/retrospectiva', [RetrospectivosRepository::class, 'create']);
+        $group->get('/retrospectiva/{id}', [RetrospectivosRepository::class, 'detail']);
+        $group->put('/retrospectiva/{id}', [RetrospectivosRepository::class, 'update']);
+        $group->delete('/retrospectiva/{id}', [RetrospectivosRepository::class, 'delete']);
+
+        $group->get('/retrospectiva/{id}/items', [ItemsRepository::class, 'list']);
+        $group->post('/retrospectiva/{id}/items', [ItemsRepository::class, 'create']);
+        $group->put('/item/{id}', [ItemsRepository::class, 'update']);
+        $group->delete('/item/{id}', [ItemsRepository::class, 'delete']);
+
+        $group->get('/retrospectiva/{id}/acciones-anteriores', [SeguimientoRepository::class, 'accionesAnteriores']);
+        $group->post('/seguimiento', [SeguimientoRepository::class, 'create']);
+        $group->put('/seguimiento/{id}', [SeguimientoRepository::class, 'update']);
     });
 };
