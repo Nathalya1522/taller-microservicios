0ebc0c235e792223deed51f5f1e6097c1615adf0
Nicolas <jnjimenez@uniboyaca.edu.co>
2026-05-22 21:50:54 -0500

    actualización index php

--- microservicios/public/index.php
+++ microservicios/public/index.php
@@ -6,7 +6,7 @@ require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../app/Config/database.php';
 
 $cors      = require __DIR__ . '/../app/Presentation/Middlewares/CorsMiddleware.php';
-$endpoints = require __DIR__ . '/../app/Routers/endpoints.php';  // ← corregido
+$endpoints = require __DIR__ . '/../app/Routers/endpoints.php';
 
 $app = AppFactory::create();
 $app->addBodyParsingMiddleware();   // ← necesario para leer JSON del body
